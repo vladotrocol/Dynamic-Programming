bf7f907f0154ad55c4a11b41c581f5d7dc55b636
Vlad <vladotrocol@yahoo.com>
2012-10-02 13:49:43 +0100

    a bit of style + zoom

--- Script.js
+++ Script.js
@@ -69,11 +69,22 @@ function printLCS(b, X, i, j) {
     }
 }
 
-function add_code() {
+function add_code(language) {
     var CodeWrap = document.getElementById("CodeWrap");
-    var n = codeString.split("\n").length - 1;
-    var s = codeString.split("\n");
+    var str;
+    if(language=="js"){
+        str = jsCode;
+    }
+    else if(language == "c"){
+        str=cCode;
+    }
+    else if(language == "s"){
+        str=sCode;
+    }
+    var n = str.split("\n").length - 1;
+    var s = str.split("\n");
     var ol = document.createElement("ol");
+    ol.id = "List";
     CodeWrap.appendChild(ol);
     for (var i = 0; i < n; i++) {
         var li = document.createElement("li");
@@ -82,6 +93,12 @@ function add_code() {
         ol.appendChild(li);
         cd.innerText = s[i];
     }
+}
+
+function remove_code(){
+    var el = document.getElementById("CodeWrap");
+    var ol = document.getElementById("List");
+    el.removeChild(ol);
 }
 
 var pos = {
@@ -99,126 +116,128 @@ function draw_table(offsetX, offsetY) {
     var x = offsetX;
     var y = offsetY;
     canvas.width = canvas.width;
-    if (tw + 2 * zoom * 10 < canvas.width) {
-        pos.x = (canvas.width - tw) / 2
-    }
-    if (th + 2 * zoom * 10 < canvas.height) {
-        pos.y = (canvas.height - th) / 2
-    }
-    if (pos.x + x > -2 * zoom * 10 - tw + canvas.width && pos.x + x < 0 + 2 * zoom * 10) pos.x += x;
-    if (pos.y + y > -2 * zoom * 10 - th + canvas.height && pos.y + y < 0 + 2 * zoom * 10) pos.y += y;
-    ctx.fillStyle = '#00ff00';
-    ctx.font = "italic " + zoom * 4 + "px sans-serif";
-    ctx.textBaseline = 'top';
-    ctx.fillText("y[j]", pos.x + zoom * 5 / 2, pos.y - zoom * 5 - 2);
-    ctx.fillText("x[i]", pos.x - ctx.measureText("x[i]").width - 4, pos.y + zoom * 5 / 2);
-    ctx.fillStyle = '#0000ff';
-    ctx.font = "italic " + zoom * 4 + "px sans-serif";
-    ctx.fillText("j", pos.x - ctx.measureText("j").width - 8, pos.y - 2 * zoom * 5 - 2);
-    ctx.fillText("i", pos.x - zoom * 10, pos.y - zoom * 5);
-
-    for (var it in Path) {
-        ctx.fillStyle = "ccc";
-        ctx.fillRect(Path[it].j * zoom * 10 + pos.x, Path[it].i * 10 * zoom + pos.y, zoom * 10, zoom * 10);
-    }
+    if(s1.length!=0&&s2.length!=0){
+        if (tw + 2 * zoom * 10 < canvas.width) {
+            pos.x = (canvas.width - tw) / 2
+        }
+        if (th + 2 * zoom * 10 < canvas.height) {
+            pos.y = (canvas.height - th) / 2
+        }
+        if (pos.x + x > -2 * zoom * 10 - tw + canvas.width && pos.x + x < 0 + 2 * zoom * 10) pos.x += x;
+        if (pos.y + y > -2 * zoom * 10 - th + canvas.height && pos.y + y < 0 + 2 * zoom * 10) pos.y += y;
+        ctx.fillStyle = '#009900';
+        ctx.font = "italic " + zoom * 4 + "px sans-serif";
+        ctx.textBaseline = 'top';
+        ctx.fillText("y[j]", pos.x + zoom * 5 / 2, pos.y - zoom * 5 - 2);
+        ctx.fillText("x[i]", pos.x - ctx.measureText("x[i]").width - 4, pos.y + zoom * 5 / 2);
+        ctx.fillStyle = '#33abd1';
+        ctx.font = "italic " + zoom * 4 + "px sans-serif";
+        ctx.fillText("j", pos.x - ctx.measureText("j").width - 8, pos.y - 2 * zoom * 5 - 2);
+        ctx.fillText("i", pos.x - zoom * 10, pos.y - zoom * 5);
 
         for (var it in Path) {
-        if(Path[it].t=="{"){
-            ctx.fillStyle = "#ff0";
-            ctx.lineWidth= 0;
-            ctx.beginPath();
-            ctx.arc(Path[it].j * zoom * 10 + pos.x+30*zoom/4, Path[it].i * 10 * zoom + pos.y+30*zoom/4,5*zoom/2, 0, 2*Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-
-            ctx.beginPath();
-            ctx.arc(Path[it].j * zoom * 10 + pos.x+4*zoom, -5 * zoom/2 + pos.y -2,5*zoom/2, 0, 2*Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
-
-            ctx.beginPath();
-            ctx.arc(pos.x-5*zoom/2,Path[it].i*zoom*10+pos.y+5*zoom+2,5*zoom/2, 0, 2*Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
+            ctx.fillStyle = "ccc";
+            ctx.fillRect(Path[it].j * zoom * 10 + pos.x, Path[it].i * 10 * zoom + pos.y, zoom * 10, zoom * 10);
         }
-    }
 
-    for (var i = 0; i <= m; i++) {
-        var ii = i * 10 * zoom + pos.y;
-        if (ii > -zoom * 10) {
-            if (ii < canvas.height + zoom * 10) {
-                var txtSize = 0;
-                for (var j = 0; j <= n; j++) {
-                    var jj = j * 10 * zoom + pos.x;
-                    if(a.c[i][j]<=9){
-                        txtSize = zoom*5;
-                    }else if (a.c[i][j]<=99){
-                        txtSize = zoom*5/2;
-                    }
-                    else{
-                       txtSize = 3*zoom*5/7;
-                    }
-                    if (jj > -zoom * 10) {
-                        if (jj < canvas.width + zoom * 10) {
-                            ctx.font = "italic " + txtSize + "px sans-serif";
-                            ctx.strokeStyle = "rgba(0, 0 ,0, 1)";
-                            ctx.lineWidth = 1;
-                            ctx.strokeRect(jj, ii, 10 * zoom, 10 * zoom);
-                            ctx.fillStyle = '#ff0000';
-                            ctx.fillText(a.c[i][j], pos.x + (j + 1) * zoom * 10 - ctx.measureText(a.c[i][j]).width - 4, pos.y + (i + 1) * zoom * 10 -txtSize - 2);
-                            ctx.fillStyle = '#00ff00';
-                            if (j < n) {
-                                ctx.fillText(s2[j], (j + 1) * zoom * 10 + pos.x + zoom * 5 / 2, pos.y - zoom * 5 - 4);
-                            }
-                            ctx.fillStyle = '#0000ff';
-                            ctx.font = "italic " + zoom * 4 + "px sans-serif";
-                            ctx.fillText(j, jj + zoom * 5 / 2, pos.y - 2 * zoom * 5 - 2);
+            for (var it in Path) {
+            if(Path[it].t=="{"){
+                ctx.fillStyle = "rgba(255, 255, 0, .5)";
+                ctx.lineWidth= 0;
+                ctx.beginPath();
+                ctx.arc(Path[it].j * zoom * 10 + pos.x+30*zoom/4, Path[it].i * 10 * zoom + pos.y+30*zoom/4,5*zoom/2, 0, 2*Math.PI, false);
+                ctx.fill();
+                ctx.closePath();
 
-                            ctx.lineWidth = Math.round(zoom / 3);
-                            ctx.strokeStyle = "rgba(255, 20 ,189, 1)";
-                            if (a.b[i][j] == "-") {
-                                ctx.beginPath();
-                                ctx.moveTo(jj + 5 * zoom, ii + 7 * zoom);
-                                ctx.lineTo(jj + zoom, ii + 7 * zoom);
-                                ctx.lineTo(jj + 3 * zoom, ii + 6 * zoom);
-                                ctx.moveTo(jj + zoom, ii + 7 * zoom);
-                                ctx.lineTo(jj + 3 * zoom, ii + 8 * zoom);
-                                ctx.closePath();
-                                ctx.stroke();
-                            } else if (a.b[i][j] == "|") {
-                                ctx.beginPath();
-                                ctx.moveTo(jj + 7 * zoom, ii + 5 * zoom);
-                                ctx.lineTo(jj + 7 * zoom, ii + zoom);
-                                ctx.lineTo(jj + 6 * zoom, ii + 3 * zoom);
-                                ctx.moveTo(jj + 7 * zoom, ii + zoom);
-                                ctx.lineTo(jj + 8 * zoom, ii + 3 * zoom);
-                                ctx.closePath();
-                                ctx.stroke();
-                            } else if (a.b[i][j] == "{") {
-                                ctx.beginPath();
-                                ctx.moveTo((jj) + 5 * zoom, ii + 5 * zoom);
-                                ctx.lineTo(jj + zoom, ii + zoom);
-                                ctx.lineTo(jj + zoom, ii + 3 * zoom);
-                                ctx.moveTo(jj + 3 *  zoom, ii + zoom);
-                                ctx.lineTo(jj + zoom, ii + zoom);
-                                ctx.closePath();
-                                ctx.stroke();
+                ctx.beginPath();
+                ctx.arc(Path[it].j * zoom * 10 + pos.x+4*zoom, -5 * zoom/2 + pos.y -2,5*zoom/2, 0, 2*Math.PI, false);
+                ctx.fill();
+                ctx.closePath();
+
+                ctx.beginPath();
+                ctx.arc(pos.x-5*zoom/2,Path[it].i*zoom*10+pos.y+5*zoom+2,5*zoom/2, 0, 2*Math.PI, false);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+
+        for (var i = 0; i <= m; i++) {
+            var ii = i * 10 * zoom + pos.y;
+            if (ii > -zoom * 10) {
+                if (ii < canvas.height + zoom * 10) {
+                    var txtSize = 0;
+                    for (var j = 0; j <= n; j++) {
+                        var jj = j * 10 * zoom + pos.x;
+                        if(a.c[i][j]<=9){
+                            txtSize = zoom*5;
+                        }else if (a.c[i][j]<=99){
+                            txtSize = zoom*5/2;
+                        }
+                        else{
+                           txtSize = 3*zoom*5/7;
+                        }
+                        if (jj > -2*zoom * 10) {
+                            if (jj < canvas.width + zoom * 10) {
+                                ctx.font = "italic " + txtSize + "px sans-serif";
+                                ctx.strokeStyle = "rgba(0, 0 ,0, 1)";
+                                ctx.lineWidth = 1;
+                                ctx.strokeRect(jj, ii, 10 * zoom, 10 * zoom);
+                                ctx.fillStyle = '#AA1414';
+                                ctx.fillText(a.c[i][j], pos.x + (j + 1) * zoom * 10 - ctx.measureText(a.c[i][j]).width - 4, pos.y + (i + 1) * zoom * 10 -txtSize - 2);
+                                ctx.fillStyle = '#009900';
+                                if (j < n) {
+                                    ctx.fillText(s2[j], (j + 1) * zoom * 10 + pos.x + zoom * 5 / 2, pos.y - zoom * 5 - 4);
+                                }
+                                ctx.fillStyle = '#33abd1';
+                                ctx.font = "italic " + zoom * 4 + "px sans-serif";
+                                ctx.fillText(j, jj + zoom * 5 / 2, pos.y - 2 * zoom * 5 - 2);
+
+                                ctx.lineWidth = Math.round(zoom / 3);
+                                ctx.strokeStyle = "rgba(168, 20 ,173, 1)";
+                                if (a.b[i][j] == "-") {
+                                    ctx.beginPath();
+                                    ctx.moveTo(jj + 5 * zoom, ii + 7 * zoom);
+                                    ctx.lineTo(jj + zoom, ii + 7 * zoom);
+                                    ctx.lineTo(jj + 3 * zoom, ii + 6 * zoom);
+                                    ctx.moveTo(jj + zoom, ii + 7 * zoom);
+                                    ctx.lineTo(jj + 3 * zoom, ii + 8 * zoom);
+                                    ctx.closePath();
+                                    ctx.stroke();
+                                } else if (a.b[i][j] == "|") {
+                                    ctx.beginPath();
+                                    ctx.moveTo(jj + 7 * zoom, ii + 5 * zoom);
+                                    ctx.lineTo(jj + 7 * zoom, ii + zoom);
+                                    ctx.lineTo(jj + 6 * zoom, ii + 3 * zoom);
+                                    ctx.moveTo(jj + 7 * zoom, ii + zoom);
+                                    ctx.lineTo(jj + 8 * zoom, ii + 3 * zoom);
+                                    ctx.closePath();
+                                    ctx.stroke();
+                                } else if (a.b[i][j] == "{") {
+                                    ctx.beginPath();
+                                    ctx.moveTo((jj) + 5 * zoom, ii + 5 * zoom);
+                                    ctx.lineTo(jj + zoom, ii + zoom);
+                                    ctx.lineTo(jj + zoom, ii + 3 * zoom);
+                                    ctx.moveTo(jj + 3 *  zoom, ii + zoom);
+                                    ctx.lineTo(jj + zoom, ii + zoom);
+                                    ctx.closePath();
+                                    ctx.stroke();
+                                }
+                                
+                            } else {
+                                j = n;
                             }
-                            
-                        } else {
-                            j = n;
                         }
                     }
+                    ctx.lineWidth = 1;
+                    ctx.fillText(i, pos.x - zoom * 10, ii + zoom * 5 / 2);
+                    ctx.fillStyle = '#009900';
+                    ctx.font = "italic " + zoom * 5 + "px sans-serif";
+                    if (i < m) {
+                        ctx.fillText(s1[i], pos.x - ctx.measureText(s1[i]).width - 4, (i + 1) * zoom * 10 + pos.y + zoom * 5 / 2);
+                    }
+                } else {
+                    i = m;
                 }
-                ctx.lineWidth = 1;
-                ctx.fillText(i, pos.x - zoom * 10, ii + zoom * 5 / 2);
-                ctx.fillStyle = '#00ff00';
-                ctx.font = "italic " + zoom * 5 + "px sans-serif";
-                if (i < m) {
-                    ctx.fillText(s1[i], pos.x - ctx.measureText(s1[i]).width - 4, (i + 1) * zoom * 10 + pos.y + zoom * 5 / 2);
-                }
-            } else {
-                i = m;
             }
         }
     }
@@ -257,22 +276,62 @@ window.onload = function() {
     var pace = 1;
     pos.x = 0;
     pos.y = 0;
-    add_code();
+    add_code("js");
     input1.onkeyup = function(e) {
+        pos.x=zoom*20;
+        pos.y=zoom*20;
         process(offsetX, offsetY);
     };
     input2.onkeyup = function(e) {
+        pos.x=zoom*20;
+        pos.y=zoom*20;
         process(offsetX, offsetY);
     };
+
     canvas.onmousedown = function(e) {
         drag = true;
         start_position = getMouse(e);
+        var rc = document.getElementById("CodeWrap");
+        rc.style["-moz-user-select"] = "none"; 
+        rc.style["-khtml-user-select"] = "none"; 
+        rc.style["-webkit-user-select"]= "noen"; 
+        rc.style["-o-user-select"]= "none";
     };
-    canvas.onmouseup = function(e) {
-        drag = false
+    window.onmouseup = function(e) {
+        drag = false;
+        var rc = document.getElementById("CodeWrap");
+        rc.style["-moz-user-select"] = "text"; 
+        rc.style["-khtml-user-select"] = "text"; 
+        rc.style["-webkit-user-select"]= "text"; 
+        rc.style["-o-user-select"]= "text";
+        var rcc = document.getElementById("Sequence");
+        rcc.style["-moz-user-select"] = "text"; 
+        rcc.style["-khtml-user-select"] = "text"; 
+        rcc.style["-webkit-user-select"]= "text"; 
+        rcc.style["-o-user-select"]= "text";
+        var rcc2 = document.getElementById("Number");
+        rcc2.style["-moz-user-select"] = "text"; 
+        rcc2.style["-khtml-user-select"] = "text"; 
+        rcc2.style["-webkit-user-select"]= "text"; 
+        rcc2.style["-o-user-select"]= "text";
     };
     canvas.onmouseout = function(e) {
-        drag = false
+        drag = false;
+        var rc = document.getElementById("CodeWrap");
+        rc.style["-moz-user-select"] = "none"; 
+        rc.style["-khtml-user-select"] = "none"; 
+        rc.style["-webkit-user-select"]= "none"; 
+        rc.style["-o-user-select"]= "none";
+        var rcc = document.getElementById("Sequence");
+        rcc.style["-moz-user-select"] = "none"; 
+        rcc.style["-khtml-user-select"] = "none"; 
+        rcc.style["-webkit-user-select"]= "none"; 
+        rcc.style["-o-user-select"]= "none";
+        var rcc2 = document.getElementById("Number");
+        rcc2.style["-moz-user-select"] = "none"; 
+        rcc2.style["-khtml-user-select"] = "none"; 
+        rcc2.style["-webkit-user-select"]= "none"; 
+        rcc2.style["-o-user-select"]= "none";
     };
     canvas.onmousemove = function(e) {
         if (drag == true) {
@@ -298,18 +357,31 @@ window.onload = function() {
         }
     };
 
+    document.getElementById("javascript").onmouseup=function(e){
+        remove_code();
+        add_code("js");
+    };
+    document.getElementById("C").onmouseup=function(e){
+        remove_code();
+        add_code("c");
+    };
+    document.getElementById("pseudo").onmouseup=function(e){
+        remove_code();
+        add_code("s");
+    };
+
 }
 
 function process(offsetX, offsetY) {
     s1 = document.getElementById("S1").value;
     s2 = document.getElementById("S2").value;
-    if (s1.length > 0 && s2.length > 0) {
         Out.length = 0;
         Path.length = 0;
         a = LCS_Length(s1, s2);
-        printLCS(a.b, s1, s1.length, s2.length);
+        printLCS(a.b, s1, s1.length, s2.length);   
+        var rNumber = document.getElementById("Number");
+        rNumber.innerText = a.c[s1.length][s2.length];
+        var rSequence = document.getElementById("Sequence");
+        rSequence.innerText = Out.join();
         draw_table(0, 0);
-        var rezult = document.getElementById("Rezults");
-        rezult.innerText = a.c[s1.length][s2.length] + "\n" + Out.join();
-    }
 }
